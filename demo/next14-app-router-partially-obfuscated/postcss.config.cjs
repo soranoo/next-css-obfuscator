4f203269e99c66ca4fbd2787522ee17f54f6797e
Freeman <hokingwun@gmail.com>
2024-01-16 14:31:25 +0000

    Added `next14-app-router-partially-obfuscated` Demo

---
+++ demo/next14-app-router-partially-obfuscated/postcss.config.cjs
@@ -0,0 +1,31 @@
+// @ts-ignore
+const isObfscMode = process.env.NODE_ENV === "obfuscation";
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "next-css-obfuscator/patched-postcss-obfuscator": {
+      enable: isObfscMode,
+      extensions: [".jsx", ".tsx", ".js", ".ts", ".html", ".rsc"],
+      formatJson: true, // 👈 must be true
+      keepData: true, // 👈 must be true
+      whiteListedPaths: [],
+      blackListedPaths: ["./next/cache"],
+      excludeAnyMatchRegex: [
+        /\.next\/server\/pages\/api/,
+        /_document..*js/,
+        /_app-.*/,
+      ],
+      classIgnore: ["static"],
+      enableObfuscateMarkerClasses: true,
+      obfuscateMarkerClasses: ["next-css-obfuscation"],
+      removeObfuscateMarkerClasses: true,
+      logLevel: "debug",
+      callBack: function () {
+        // @ts-ignore
+        process.env.NODE_ENV = "production"; // to make sure postcss-obfuscator doesn't re-run.
+      },
+    },
+  },
+};
---
+++ demo/next14-app-router-partially-obfuscated/src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#fac3e3] to-[#5c9cbd] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Next14 App Router
+        </h1>
+      </div>
+      <div className="next-css-obfuscation container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <span className="text-2xl font-extrabold tracking-tight text-gray-200">
+          My classes is obfuscated - let out the DevTools
+        </span>
+      </div>
+    </main>
+  );
+}
