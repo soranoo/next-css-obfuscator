06d98cc616194b26c4fe564010d39bb397d62fe0
Freeman <46896789+soranoo@users.noreply.github.com>
2025-04-25 12:56:16 +0100

    fix: update release-notes-generator configuration to use angular preset

--- release.config.cjs
+++ release.config.cjs
@@ -1,18 +1,37 @@
 module.exports = {
   branches: [{ name: "main" }, { name: "beta", prerelease: true }],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "parserOpts": {
+          "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        "releaseRules": [
+          { type: "feat", release: "major" },
+          { type: "fix", release: "major" },
+          { type: "perf", release: "major" },
+          { type: "BREAKING CHANGE", release: "major" },
+          { type: "docs", release: false },
+          { type: "style", release: false },
+          { type: "refactor", release: false },
+          { type: "test", release: false },
+          { type: "chore", release: false },
+        ],
+      }
+    ],
     [
       "@semantic-release/release-notes-generator",
       {
-        preset: "conventionalcommits",
-        releaseRules: [
-          { type: "feat", release: "major" },
-          { type: "fix", release: "major" },
-          { type: "perf", release: "patch" },
-          { type: "BREAKING CHANGE", release: "major" },
-        ],
-      },
+        "preset": "angular",
+        "parserOpts": {
+          "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        "writerOpts": {
+          "commitsSort": ["subject", "scope"]
+        }
+      }
     ],
     "@semantic-release/npm",
     [
