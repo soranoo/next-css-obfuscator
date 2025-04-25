22d30af5dfa90bee86e5f9768b05c00874bb4732
Freeman <46896789+soranoo@users.noreply.github.com>
2025-04-25 12:21:50 +0100

    ci: simplify release notes generator configuration

--- release.config.cjs
+++ release.config.cjs
@@ -2,18 +2,7 @@ module.exports = {
   branches: [{ name: "main" }, { name: "beta", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "conventionalcommits",
-        releaseRules: [
-          { type: "feat", release: "minor" },
-          { type: "fix", release: "patch" },
-          { type: "perf", release: "patch" },
-          { type: "BREAKING CHANGE", release: "major" },
-        ],
-      },
-    ],
+    "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     [
       "@semantic-release/git",
