fabcd155e521cc9538e5085eeb8d938f6ba7f437
Freeman <46896789+soranoo@users.noreply.github.com>
2025-04-25 11:23:52 +0100

    ci: Update release workflow to handle push and pull request events, add dry run step

--- release.config.cjs
+++ release.config.cjs
@@ -3,13 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/npm",
-      {
-        npmPublish: true,
-        pkgRoot: "dist",
-      },
-    ],
+    "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
