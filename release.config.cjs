0d1d28f26c027c9326c7f5b5cd57b46cf52f255b
Freeman <46896789+soranoo@users.noreply.github.com>
2025-04-25 12:52:33 +0100

    fix: restructure release-notes-generator configuration in release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,18 @@ module.exports = {
   branches: [{ name: "main" }, { name: "beta", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "feat", release: "major" },
+          { type: "fix", release: "major" },
+          { type: "perf", release: "patch" },
+          { type: "BREAKING CHANGE", release: "major" },
+        ],
+      },
+    ],
     "@semantic-release/npm",
     [
       "@semantic-release/git",
