3ff09968cf5d84aca163dc7b1f8d2024d6dd4d8a
Freeman <46896789+soranoo@users.noreply.github.com>
2025-04-25 13:11:48 +0100

    BREAKING CHANGE: force version upgarde
    BREAKING CHANGE
    force version upgarde

--- release.config.cjs
+++ release.config.cjs
@@ -8,6 +8,17 @@ module.exports = {
         "parserOpts": {
           "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
         },
+        "releaseRules": [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "BREAKING CHANGE", release: "major" },
+          { type: "docs", release: false },
+          { type: "style", release: false },
+          { type: "refactor", release: false },
+          { type: "test", release: false },
+          { type: "chore", release: false },
+        ],
       }
     ],
     [
